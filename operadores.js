c018ca785faf5db3b0510220b4e3283cd2fef411
DEAlvarezLopez <daniexdaniel@gmail.com>
2023-01-21 22:20:08 -0400

    operadores y variables 2

--- operadores.js
+++ operadores.js
@@ -1,2 +1,12 @@
-console.log (4>7)
-console.log (5<10)
+console.log (4>7);
+console.log (5<10);
+console.log (10/2/5);
+
+var a = 3;
+var b = 10;
+var c = a = b;
+console.log (c);
+
+console.log (4==4);
+console.log (4=='4');
+console.log (4==='4');
