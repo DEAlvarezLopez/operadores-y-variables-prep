1899e2ee24a155cc7d787bdfbcfe42d54f61ceb2
DEAlvarezLopez <daniexdaniel@gmail.com>
2023-01-21 23:57:03 -0400

    control de flujo prep

---
+++ controlDeFlujo.js
@@ -0,0 +1,7 @@
+function practicarGuitarra(dia) {
+    if (dia === 'lunes') {console.log ('descansa')}
+    else if (dia === 'miercoles') {console.log ('descansa')}
+    else if (dia === 'viernes') {console.log ('descansa')}
+    else {console.log('practica'),''}
+} ;
+practicarGuitarra ('domingo')
