b9c2fbe74d73fbf3642a9c536726241cbbf05114
DEAlvarezLopez <daniexdaniel@gmail.com>
2023-01-26 16:11:35 -0400

    actualización

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var listaDeCompras = [];
+listaDeCompras [3] = 'Tomates';
+listaDeCompras [1] = 'Lechuga';
+
+//console.log (listaDeCompras);
+
+//listaDeCompras [1];
+
+/* var elementoDelArray = listaDeCompras [1];
+console.log (elementoDelArray); */
+
+//length
+
+//console.log (listaDeCompras.length)
---
+++ bucle.js
@@ -0,0 +1,43 @@
+//var suma = 0;
+
+//for (var i=0 ; i<10; i++){
+//    suma = suma +1;
+//   console.log (suma)
+//}
+
+//for (var i=0 ; i<10 ; i++) {
+//    suma = suma + 1;
+//    console.log ('variable de iteracion', i);
+//}
+//console.log (suma)
+
+/*while (suma < 20) {
+    suma = suma +1;
+    console.log(suma);
+}*/
+
+//for
+
+/*function encontrarLetraP (string) {
+    var letras = string.split('');
+
+    for (let i = 0; i < letras.length; i++){
+        if (letras [i] === 'p') {
+            console.log('SI contiene la P');
+            console.log ('no contiene')
+            } 
+    }
+}
+
+encontrarLetraP ('JavaScript')
+encontrarLetraP ('Henry')*/
+
+//while
+
+var arr = [];
+
+while  (arr.length < 5){
+    arr.push('BOOM');
+}
+
+console.log(arr)
